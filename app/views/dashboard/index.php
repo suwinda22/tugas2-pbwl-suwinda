4048e7dc8efedb94031704e4927592e905cfbe88
suwinda22 <suwinda0610@gmail.com>
2023-06-20 20:47:33 +0700

    first

---
+++ app/views/dashboard/index.php
@@ -0,0 +1,5 @@
+<h2>Dashboard</h2>
+
+<div class="info">Selamat datang, <strong>Suwinda Aulansari</strong></div>
+
+<div class="info"> Ini merupakan website yang digunakan untuk menginputkan data golongan, data user dan data pelanggan. Pada website ini kita juga dapat mengedit data golongan, data user dan data pelanggan.</div>
